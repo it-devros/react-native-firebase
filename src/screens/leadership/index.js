2d3441c4c12115a3fb22add873a5141a0c3a8311
it-devros <it-devros@outlook.com>
2019-06-26 15:14:57 -0400

    leadership screen updated

--- src/screens/leadership/index.js
+++ src/screens/leadership/index.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import {
   View,
-  Text
+  Text,
+  ScrollView,
+  Image,
+  ImageBackground,
+  TouchableOpacity
 } from 'react-native'
 
 import { bindActionCreators } from 'redux'
@@ -43,8 +47,82 @@ class Leadership extends React.Component {
   render() {
 
     return(
-      <View>
-        <Text>This is Leadership</Text>
+      <View style={styles.container}>
+        <View style={styles.topSection}>
+          <Text style={styles.topTitle}>CONNECT WITH OTHERS IN YOUR LOCAL GROUPS</Text>
+        </View>
+        <ScrollView>
+          <View style={styles.yourGroupSection}>
+            <Text style={styles.yourGroupTitle}>Your Groups</Text>
+            <View style={styles.cardsSection}>
+              <View style={styles.card}>
+                <Image
+                  style={styles.coverImage}
+                  source={require('../../assets/images/cover.jpg')}
+                />
+                <View style={styles.cardContent}>
+                  <Text style={styles.contentText}>
+                    PUBLIC GROUP RALEIGH Bringing all Raleigh family caregivers together for support, shared connections and relief
+                  </Text>
+                  <View style={styles.cardFooter}>
+                    <Text style={styles.contentText}>1382 Caregivers Sharing</Text>
+                    <View style={styles.postButton}>
+                      <Text style={styles.postBtnTitle}>68 posts</Text>
+                    </View>
+                  </View>
+                </View>
+              </View>
+            </View>
+          </View>
+          <View style={styles.otherGroupSection}>
+            <Text style={styles.otherGroupTitle}>Other groups you may be interested in</Text>
+            <View style={styles.card}>
+              <Image
+                style={styles.coverImage}
+                source={require('../../assets/images/cover.jpg')}
+              />
+              <View style={styles.cardContent}>
+                <Text style={styles.contentText}>
+                  PUBLIC GROUP RALEIGH Bringing all Raleigh family caregivers together for support, shared connections and relief
+                </Text>
+                <View style={styles.cardFooter}>
+                  <Text style={styles.contentText}>1382 Caregivers Sharing</Text>
+                  <View style={styles.postButton}>
+                    <Text style={styles.postBtnTitle}>68 posts</Text>
+                  </View>
+                </View>
+              </View>
+            </View>
+            <View style={styles.card}>
+              <Image
+                style={styles.coverImage}
+                source={require('../../assets/images/cover.jpg')}
+              />
+              <View style={styles.cardContent}>
+                <Text style={styles.contentText}>
+                  PUBLIC GROUP RALEIGH Bringing all Raleigh family caregivers together for support, shared connections and relief
+                </Text>
+                <View style={styles.cardFooter}>
+                  <Text style={styles.contentText}>1382 Caregivers Sharing</Text>
+                  <View style={styles.postButton}>
+                    <Text style={styles.postBtnTitle}>68 posts</Text>
+                  </View>
+                </View>
+              </View>
+            </View>
+
+            <View style={styles.addGroupButtonSection}>
+              <TouchableOpacity style={styles.buttonItem}>
+                <View style={styles.buttonCircle}>
+                  <Image
+                    style={styles.buttonImage}
+                    source={require('../../assets/icons/plus.png')}
+                  />
+                </View>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </ScrollView>
       </View>
     )
 
--- src/screens/leadership/style.js
+++ src/screens/leadership/style.js
@@ -11,7 +11,119 @@ import {
 const { height, width } = Dimensions.get('window')
 
 const styles = StyleSheet.create({
-
+  container: {
+    width: width
+  },
+  topSection: {
+    width: '100%',
+    padding: normalize(10),
+    borderBottomWidth: 2,
+    borderColor: 'rgba(112,112,112, 0.5)'
+  },
+  topTitle: {
+    fontSize: normalize(18),
+    fontWeight: 'bold',
+    color: '#000000',
+    textAlign: 'center'
+  },
+  yourGroupSection: {
+    padding: normalize(10),
+    borderBottomWidth: normalize(2),
+    borderColor: 'rgba(112,112,112, 0.5)'
+  },
+  yourGroupTitle: {
+    fontSize: normalize(18),
+    fontWeight: 'bold',
+    color: '#000000',
+    textAlign: 'left'
+  },
+  cardsSection: {
+    padding: normalize(10)
+  },
+  card: {
+    position: 'relative',
+    width: '100%',
+    borderColor: 'rgba(112,112,112, 0.5)',
+    borderWidth: normalize(2),
+    borderRadius: normalize(5),
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 0,
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 10,
+    elevation: 10,
+    marginTop: normalize(5),
+    marginBottom: normalize(5)
+  },
+  coverImage: {
+    width: '100%',
+    height: (width - normalize(40)) / 1.6,
+    borderRadius: normalize(3),
+  },
+  cardContent: {
+    position: 'absolute',
+    width: '100%',
+    height: (width - normalize(40)) / 1.6,
+    borderRadius: normalize(3),
+    backgroundColor: 'rgba(0,0,0, 0.2)',
+    left: 0,
+    top: 0,
+    padding: normalize(10),
+    justifyContent: 'space-between'
+  },
+  contentText: {
+    color: '#ffffff'
+  },
+  cardFooter: {
+    flexDirection: 'row',
+    alignItems: 'flex-end',
+    justifyContent: 'space-between'
+  },
+  postButton: {
+    borderColor: 'rgba(112,112,112, 0.5)',
+    borderWidth: normalize(2),
+    borderRadius: normalize(7),
+    padding: normalize(5),
+    backgroundColor: 'rgba(84,12,105, 0.5)'
+  },
+  postBtnTitle: {
+    color: '#ffffff',
+    fontSize: normalize(16)
+  },
+  otherGroupSection: {
+    padding: normalize(20),
+    paddingBottom: normalize(100)
+  },
+  otherGroupTitle: {
+    fontSize: normalize(16),
+    color: '#000000',
+    textAlign: 'left',
+    marginBottom: normalize(10)
+  },
+  addGroupButtonSection: {
+    paddingTop: normalize(30),
+    alignItems: 'center'
+  },
+  buttonItem: {
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  buttonCircle: {
+    width: normalize(60),
+    height: normalize(60),
+    borderRadius: normalize(30),
+    borderWidth: normalize(3),
+    borderColor: 'rgba(112,112,112, 1)',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: normalize(5)
+  },
+  buttonImage: {
+    width: normalize(30),
+    height: normalize(30)
+  },
 })
 
 

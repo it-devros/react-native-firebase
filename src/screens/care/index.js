9df4990a3b71c57abffe4107789b7e7acc1800a0
it-devros <it-devros@outlook.com>
2019-06-27 13:17:00 -0400

    care screen updated

--- src/screens/care/index.js
+++ src/screens/care/index.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import {
   View,
-  Text
+  Text,
+  Image,
+  TouchableOpacity
 } from 'react-native'
 
 import { bindActionCreators } from 'redux'
@@ -43,8 +45,55 @@ class Care extends React.Component {
   render() {
 
     return(
-      <View>
-        <Text></Text>
+      <View style={styles.container}>
+        <View style={styles.topSection}>
+          <View style={styles.navigateSection}>
+            <View style={styles.avatarSection}>
+              <Image
+                style={styles.avatarImage}
+                source={require('../../assets/avatars/avatar9.jpg')}
+              />
+            </View>
+            <View style={styles.navDropSection}>
+              <TouchableOpacity style={styles.navItem}>
+                <Image
+                  style={styles.navImage}
+                  source={require('../../assets/icons/request_care.png')}
+                />
+                <Text style={styles.navTitle}>Request Care</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.navItem}>
+                <Image
+                  style={styles.navImage}
+                  source={require('../../assets/icons/offer_care.png')}
+                />
+                <Text style={styles.navTitle}>Offer Care</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.navItem}>
+                <Image
+                  style={styles.navImage}
+                  source={require('../../assets/icons/view_super_profile.png')}
+                />
+                <Text style={styles.navTitle}>View Super Carer Profile</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+          <View style={styles.middleButtonSection}>
+            <TouchableOpacity>
+              <View style={styles.tagButton}>
+                <Text style={styles.tagButtonTitle}>REQUEST CARE</Text>
+              </View>
+            </TouchableOpacity>
+            <TouchableOpacity>
+              <View style={styles.tagButton}>
+                <Text style={styles.tagButtonTitle}>OFFER CARE</Text>
+              </View>
+            </TouchableOpacity>
+          </View>
+          <View style={styles.mapHeaderTextSection}>
+            <Text style={styles.mapHeaderText}>CAREGIVERS NEAR YOU</Text>
+          </View>
+        </View>
       </View>
     )
 
--- src/screens/care/style.js
+++ src/screens/care/style.js
@@ -11,7 +11,84 @@ import {
 const { height, width } = Dimensions.get('window')
 
 const styles = StyleSheet.create({
+  container: {
+    width: width
+  },
+  topSection: {
 
+  },
+  navigateSection: {
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    paddingBottom: normalize(15)
+  },
+  avatarSection: {
+    flex: 1,
+    padding: normalize(5)
+  },
+  avatarImage: {
+    width: normalize(60),
+    height: normalize(60),
+    marginRight: normalize(5),
+    borderRadius: normalize(30)
+  },
+  navDropSection: {
+    flex: 1,
+    padding: normalize(5)
+  },
+  navItem: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start'
+  },
+  navImage: {
+    width: normalize(30),
+    height: normalize(30),
+    marginRight: normalize(10)
+  },
+  navTitle: {
+    flex: 1,
+    fontSize: normalize(16),
+    color: '#000000',
+    textAlign: 'left'
+  },
+  middleButtonSection: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    padding: normalize(5),
+    paddingBottom: normalize(15)
+  },
+  tagButton: {
+    borderColor: 'rgba(112,112,112, 0.5)',
+    borderWidth: normalize(2),
+    borderRadius: normalize(7),
+    padding: normalize(5),
+    marginTop: normalize(5),
+    backgroundColor: '#af6d3a',
+    
+  },
+  tagButtonTitle: {
+    color: '#ffffff',
+    fontSize: normalize(18),
+    textAlign: 'center'
+  },
+  mapHeaderTextSection: {
+    borderColor: 'rgba(112,112,112, 0.5)',
+    borderTopWidth: normalize(2),
+    borderBottomWidth: normalize(2),
+    padding: normalize(10),
+    backgroundColor: '#540C69',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  mapHeaderText: {
+    fontSize: normalize(18),
+    color: '#000000',
+    textAlign: 'center'
+  }
 })
 
 
